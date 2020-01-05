c3e232255ff6279969a232c276e5108c6cf0428b
Amesmer <535026442@qq.com>
2020-01-05 10:13:08 +0800

    project init complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-addtion></my-addtion>
+    <p>------------------------------</p>
+    <my-subtraction></my-subtraction>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Addtion from './components/Addition'
+import Subtraction from './components/Subtration'
 export default {
-  name: 'app',
+  data () {
+    return {}
+  },
   components: {
-    HelloWorld
+    'my-addtion': Addtion,
+    'my-subtraction': Subtraction
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
 </style>
---
+++ src/components/Addition.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>当前的count值为:</h3>
+    <button>+1</button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
